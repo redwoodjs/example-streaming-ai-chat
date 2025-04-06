abd98dfed298f63a945aec2965f6503626572ca8
Peter Pistorius <peter.pistorius@gmail.com>
2025-04-06 17:43:20 +0200

    Improve styles.

--- src/app/pages/Chat/Chat.tsx
+++ src/app/pages/Chat/Chat.tsx
@@ -7,53 +7,51 @@ import { consumeEventStream } from "@redwoodjs/sdk/client";
 export function Chat() {
   const [message, setMessage] = useState("");
   const [reply, setReply] = useState("");
-
+  const [isLoading, setIsLoading] = useState(false);
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+
+    setIsLoading(true);
 
     setReply(""); // Reset reply before new message
     (await sendMessage(message)).pipeTo(
       consumeEventStream({
         onChunk: (event) => {
-          setReply(
-            (prev) =>
-              prev +
-              (event.data === "[DONE]" ? "" : JSON.parse(event.data).response),
-          );
+          setReply((prev) => {
+            if (event.data === "[DONE]") {
+              // stop loading...
+              setIsLoading(false);
+              return prev;
+            }
+            return (prev += JSON.parse(event.data).response);
+          });
         },
       }),
     );
   };
 
   return (
-    <form onSubmit={onSubmit}>
-      <div>{reply}</div>
-      <input
-        type="text"
-        value={message}
-        placeholder="Type a message..."
-        onChange={(e) => setMessage(e.target.value)}
-        style={{
-          width: "80%",
-          padding: "10px",
-          marginRight: "8px",
-          borderRadius: "4px",
-          border: "1px solid #ccc",
-        }}
-      />
-      <button
-        onClick={onSubmit}
-        style={{
-          padding: "10px 20px",
-          borderRadius: "4px",
-          border: "none",
-          backgroundColor: "#007bff",
-          color: "white",
-          cursor: "pointer",
-        }}
-      >
-        Send
-      </button>
-    </form>
+    <div className="flex flex-col h-screen">
+      <div className="flex-1 overflow-y-auto p-4 mx-auto min-w-6/12 max-w-6/12">
+        {reply}
+      </div>
+
+      <form onSubmit={onSubmit} className="bg-white flex rounded-lg p-4">
+        <input
+          type="text"
+          value={message}
+          placeholder="Type a message..."
+          onChange={(e) => setMessage(e.target.value)}
+          className="w-full rounded-lg border-2 border-gray-300 p-2 mr-2"
+        />
+        <button
+          type="submit"
+          disabled={message.length === 0 || isLoading}
+          className="rounded bg-amber-800 text-white px-4 py-2"
+        >
+          {isLoading ? "Sending..." : "Send"}
+        </button>
+      </form>
+    </div>
   );
 }
--- src/app/pages/Chat/functions.ts
+++ src/app/pages/Chat/functions.ts
@@ -2,10 +2,11 @@
 
 import { env } from "cloudflare:workers";
 
-export async function sendMessage(message: string) {
+export async function sendMessage(prompt: string) {
+  console.log("Running AI with Prompt:", prompt);
   // @ts-ignore: Model is not yet typed.
   const response = await env.AI.run("@cf/meta/llama-4-scout-17b-16e-instruct", {
-    prompt: message,
+    prompt,
     stream: true,
   });
   return response as unknown as ReadableStream;
