4faafd3543ea66b92e0092fd81622c21c1a92cb6
Peter Pistorius <peter.pistorius@gmail.com>
2025-04-06 17:30:25 +0200

    Switch to Llama 4.

--- src/app/pages/Chat/functions.ts
+++ src/app/pages/Chat/functions.ts
@@ -1,12 +1,12 @@
 "use server";
 
-import { HandlerOptions } from "@redwoodjs/sdk/router";
+import { env } from "cloudflare:workers";
 
-export async function sendMessage(message: string, opts?: HandlerOptions) {
-  const response = await opts!.env.AI.run("@cf/meta/llama-3.1-8b-instruct", {
+export async function sendMessage(message: string) {
+  // @ts-ignore: Model is not yet typed.
+  const response = await env.AI.run("@cf/meta/llama-4-scout-17b-16e-instruct", {
     prompt: message,
     stream: true,
   });
-
   return response as unknown as ReadableStream;
 }
