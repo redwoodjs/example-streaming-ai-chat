4c6a8a78b8c30f0d616811e30fecb7c4d4b64e77
justinvdm <justinvderm@gmail.com>
2025-04-28 11:44:56 +0200

    Upgrade to .76

--- src/app/Document.tsx
+++ src/app/Document.tsx
@@ -9,10 +9,11 @@ export const Document: React.FC<{ children: React.ReactNode }> = ({
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <title>@redwoodjs/starter-minimal</title>
       <link rel="stylesheet" href={styles} />
-      <script type="module" src="/src/client.tsx"></script>
+      <link rel="modulepreload" href="/src/client.tsx" />
     </head>
     <body>
       <div id="root">{children}</div>
+      <script>import("/src/client.tsx")</script>
     </body>
   </html>
 );
